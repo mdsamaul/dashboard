fad9c7d271c422de5f5f85c85560bcb996cecf06
Md Samaul <mdsamal843@gmail.com>
2023-03-06 15:50:48 +0600

    added

--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
 const navItems = document.getElementById('lg-navbar');
 const mobileNavber = document.getElementById('mobile-manber');
+const morebtn = document.getElementById('collapse-navbar');
+const dashboardText = document.getElementById('dashboard-text');
+
 document.getElementById('collapse-navbar').addEventListener('click', function () {
     console.log(navItems);
     if (navItems.style.display == 'none' || mobileNavber.style.display == 'none') {
         navItems.style.display = 'block'
-        mobileNavber.style.display = 'block'
+        mobileNavber.style.display = 'block';
+        dashboardText.style.display = 'block'
     } else {
         navItems.style.display = 'none'
         mobileNavber.style.display = 'none'
-        navItems.classList.add('samaulrajshovon');
 
+        dashboardText.style.display = 'none'
     }
 })
